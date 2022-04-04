23397311de50d5dfaeeafe4e14b7e46400dec0dc
Khaled-Waled <khaledw.hamed001@gmail.com>
2022-04-04 20:46:33 +0200

    Initialized the datastructures.

--- main.cpp
+++ main.cpp
@@ -21,12 +21,82 @@ void greetingMessage()
 void initDataStructures()
 {
     cout<<"*** Initializing resources...\n";
+    cout<<"How many resources are in the system?\n";
+    cin>>NUM_OF_RESOURCE_TYPES;
 
+    available = new int[NUM_OF_RESOURCE_TYPES]; //Init available[]
+
+    //Fill available[]
+    cout<<"Please enter the amount of initially available resources:\n";
+    for(int i=0; i<NUM_OF_RESOURCE_TYPES; i++)
+    {
+        printf("R%d:",i);
+        int value=0;
+        cin>>available[i];
+    }
+
+
+
+    cout<<"\n\n*** Initializing resources...\n";
+    cout<<"How many processes are in the system?\n";
+    cin>>NUM_OF_Processes;
+
+    //init maximum[][]
+    maximum = new int*[NUM_OF_Processes];
+    for (int i=0; i<NUM_OF_Processes; i++)
+        maximum[i] = new int[NUM_OF_RESOURCE_TYPES];
+
+    //init allocation[][]
+    allocation = new int*[NUM_OF_Processes];
+    for (int i=0; i<NUM_OF_Processes; i++)
+        allocation[i] = new int[NUM_OF_RESOURCE_TYPES];
+
+    //init need[][]
+    need = new int*[NUM_OF_Processes];
+    for (int i=0; i<NUM_OF_Processes; i++)
+        need[i] = new int[NUM_OF_RESOURCE_TYPES];
+
+
+    //fill maximum[][] and allocation[][] and need[][]
+    cout<<"For each process, enter resource info for each resource\n";
+    cout<<"space separated like: R0 R1 R2...\n";
+    for (int i=0; i<NUM_OF_Processes; i++)
+    {
+        //fill maximum[][]
+        cout<<"Process #"<<i<<" has maximum resources needs of: \n";
+        for (int j=0; i<NUM_OF_RESOURCE_TYPES; i++)
+           cin>>maximum[i][j];
+
+        //fill allocation[][]
+        cout<<"\nProcess #"<<i<<" is already using resources of: \n";
+        for (int j=0; i<NUM_OF_RESOURCE_TYPES; i++)
+            cin>>allocation[i][j];
+
+        //fill need[][]
+        for (int j=0; i<NUM_OF_RESOURCE_TYPES; i++)
+            need[i][j] = maximum[i][j] - allocation[i][j];
+    }
 }
 
+bool isSafe(int pid, int request[NUM_OF_RESOURCE_TYPES])
+{
+    //add request in the right place and then check using banker algorithm
+    //TODO @Islam
+    return false;
+}
+
+//DONT RUN unless fixed!
+void recover()
+{
+    if (isSafe) return;
+    else recover();
+    //TODO @Hagry
+}
 
 int main()
 {
     greetingMessage();
     initDataStructures();
+
+
 }
